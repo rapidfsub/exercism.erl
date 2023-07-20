ae5b03d459acd8a1075bdba40a7b88be7eff633e
rapidfsub <rapidfsub@gmail.com>
2023-07-20 12:30:55 +0900

    feat: solve

--- series/src/series.erl
+++ series/src/series.erl
@@ -2,5 +2,15 @@
 
 -export([slices/2]).
 
+slices(SliceLength, Series) ->
+  Length = length(Series),
+  if Length < SliceLength ->
+       error(badarg);
+     true ->
+       do_slices(Length - SliceLength + 1, SliceLength, Series)
+  end.
 
-slices(_SliceLength, _Series) -> undefined.
+do_slices(0, _SliceLength, _Series) ->
+  [];
+do_slices(Count, SliceLength, [_Head | Tail] = Series) ->
+  [lists:sublist(Series, SliceLength) | do_slices(Count - 1, SliceLength, Tail)].
