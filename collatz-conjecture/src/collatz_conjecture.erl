c384a91202e238771b4242863fb2db50c6f23ba6
rapidfsub <rapidfsub@gmail.com>
2023-07-20 09:14:11 +0900

    feat: solve

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -2,5 +2,15 @@
 
 -export([steps/1]).
 
-
-steps(_N) -> undefined.
+steps(N) when N > 1 ->
+  case N rem 2 of
+    0 ->
+      steps(N div 2);
+    _ ->
+      steps(3 * N + 1)
+  end
+  + 1;
+steps(1) ->
+  0;
+steps(_N) ->
+  error(badarg).
